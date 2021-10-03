23db26eab7f364e5cf3c2b91bb66136ee4e7ddf7
fade ahmad <ffaaddee607122@gmail.com>
2021-10-03 11:28:55 +0530

    first commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-73=n_dk@akbk0ko=rdj08h*q5+9x_!!yttk8q%e#y^_&5#36qy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','fade-project.herokuapp.com/']
 
 
 # Application definition
@@ -96,6 +96,7 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
 
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('api/products/',include('base.urls.product_urls')),
     path('api/users/',include('base.urls.user_urls')),
     path('api/orders/',include('base.urls.order_urls')),
+   
 ]
 
 
